879922931c83eb51a2f421aad414c378323afcef
Dat Nguyen <=>
2025-05-01 16:58:25 +0700

    feat: add radio button for choosing topping item

--- src/components/dish/ToppingItemCheckBox.jsx
+++ src/components/dish/ToppingItemCheckBox.jsx
@@ -1,27 +1,19 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
-const ToppingItem = ({ topping, cartItem, handleChooseTopping }) => {
-  const [checked, setChecked] = useState(false);
-
-  useEffect(() => {
-    const hasTopping = cartItem?.toppings?.some((tp) => tp._id === topping._id);
-
-    setChecked(hasTopping);
-  }, [cartItem]);
-
+const ToppingItemCheckBox = ({ topping, toppingGroup, selectedTopping, handleChooseTopping }) => {
   return (
     <div
       className='flex items-center justify-between py-[20px] cursor-pointer'
       name='checkedBtn'
       style={{ borderBottom: "1px solid #a3a3a3a3" }}
       onClick={() => {
-        handleChooseTopping(topping, topping.price);
+        handleChooseTopping(topping, topping.price, toppingGroup);
         setChecked(!checked);
       }}
     >
       <div className='flex items-center gap-[20px]'>
-        {checked ? (
+        {selectedTopping?.some((tp) => tp._id === topping._id) ? (
           <Image src='/assets/check_box_checked.png' className='checked' alt='' width={21} height={21} />
         ) : (
           <Image src='/assets/check_box_empty.png' className='unchecked' alt='' width={20} height={20} />
@@ -38,4 +30,4 @@ const ToppingItem = ({ topping, cartItem, handleChooseTopping }) => {
   );
 };
 
-export default ToppingItem;
+export default ToppingItemCheckBox;
